87c17855b52e9f6ac6a0906b10f8c6ae7994da24
Bohdan Klymov <programmer.b.k.339@gmail.com>
2025-11-21 10:48:50 +0100

    Add files via upload
    Field output has been changed.
    
    The program checks whether input is an integer and whether the number of columns and rows is less than 4 and greater than 0.
    
    Converts the type after input from a string to an integer.

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,55 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Tic-tac-toe game:");
-        System.out.print("You need to use 'x'");
+        System.out.println("You will automatically use 'x'.");
+        System.out.println("Write down two numbers. The first number is the position of “x” in the column, and the second is in the row. \nYour options for each method: 1-3.");
 
-        String[][] filds = {
-                {"\n", "*", "*", "*", "\n"},
-                {"*", "*", "*", "\n"},
-                {"*", "*", "*", "\n"}
+        String[][] fields = {
+                {"*", "*", "*"},
+                {"*", "*", "*"},
+                {"*", "*", "*"}
         };
 
-        for (int i = 0; i < filds.length; i++) {
-            for (int j = 0; j < filds[i].length; j++) {
-                System.out.print(filds[i][j] + " ");
+        for (String[] row : fields) {
+            for (String element : row) {
+                System.out.print(element + " ");
             }
+            System.out.println();
         }
 
         Scanner userInput = new Scanner(System.in);
         boolean endPoint = false;
 
         while (!endPoint){
-            System.out.print("It's your turn: ");
-            String userTurn = userInput.nextLine();
+            System.out.print("Position in column: ");
+            String column = userInput.nextLine().trim();;
 
-            if (userTurn.equals("x")) {
-                System.out.println("You printed 'x'");
-                endPoint = true;
+            System.out.print("Position in row: ");
+            String row = userInput.nextLine().trim();;
+
+            if (isInteger(column) || isInteger(row)) {
+                if (Integer.parseInt(column) < 4 || Integer.parseInt(column) > 0 || Integer.parseInt(row) < 4 || Integer.parseInt(row) > 0) {
+
+                }
+                else {
+                    System.out.println("Your options for each method: 1-3.");
+                }
             }
+            else {
+                System.out.println("You can only write numbers.");
+            }
+
         }
         userInput.close();
+    }
+
+    public static boolean isInteger(String s) {
+        if (s == null || s.isEmpty()) return false;
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
